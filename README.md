e6d7c1a3f9d87cf3905e77e5fa5dcde7ebf1188c
jesssullivan <Jesss@sulliwood.org>
2020-01-12 00:55:02 -0500

    + logger, + readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# momo
+# MOMO:
+
+*A Simple Brake / Throttle controller w/ Arduino Leonardo- D&M WIP by Jess Sullivan*
+    
+***extra bits regarding cmake & compilation:***     
+    
+*USB port must be accessible to $USER:*
+```shell script
+
+# to check permissions on port:
+ls -l /dev/ttyACM*
+
+# add $USER to rw group of port, usually group 'dialout':
+sudo adduser $USER dialout
+
+# to change permissions at port:
+sudo chmod a+rw /dev/ttyACM0
+```
+    
+intellij
+*Regarding cmake, intellij, etc-
+the standard arduino executable + gui is still preferred for actually uploading to arduino hardware*
+    
+```shell script
+# get ide here, at least for uploading arduino .elf:
+# https://www.arduino.cc/en/Main/Software
+
+# something along the lines of:
+tar xvf arduino-1.8.10/arduino-1.8.10-linux64.tar.xz 
+chmod u+x arduino-1.8.10/install.sh
+sudo ./arduino-1.8.10/install.sh
+
+# using avrdude --> atmega: (unlikely to work well)
+avrdude -C/../avrdude.conf -v -patmega328p -carduino -P/dev/my_com_port -b57600 -D -Uflash:w:/...my_project.hex:i
+``` 
+    
+*retrieve HID depends:*
+```shell script
+# std arduino usb library:
+wget https://raw.githubusercontent.com/arduino/ArduinoCore-samd/master/cores/arduino/USB/PluggableUSB.h
+# pluggable depend:
+wget https://raw.githubusercontent.com/omnidan/ArduinoPure/master/arduino/USBAPI.h
+```
+    
+*.elf --> hex:*
+``` shell script
+avr-objcopy -j .text -j .data -O ihex my_project.elf my_project.hex
+```     
+    
--- momo.ino
+++ momo.ino
@@ -1,48 +1,66 @@
+/****************************************************************
+ * Split potentiometer Brake / Throttle controller w/ Arduino Leonardo
+ * 
+ * a WIP by for the D&M Makerspace @ https://github.com/jesssullivan
+ * 
+ * joystick library (rewrap of pluggableusb) available here:
+ * https://github.com/MHeironimus/ArduinoJoystickLibrary
+*****************************************************************/
+
+
 #include <Joystick.h>
+bool logs = true;  // use Serial monitor?
+
 
 // pinout:
 int dual = A0;
+
 
 // generic:
 int pinValue = 0;
 int pinByte = 0;
 
-// weird name convention here, not sure about usage of class_
+
+// initialize a new instance of Joystick_:
 Joystick_ Joystick;
 
-void setup() {
-    Joystick.begin();
-}
 
-void setPin(int pin, int split=127) {
-    pinValue = analogRead(pin);
-    if (pinValue > 0) {
-        pinByte = pinValue / 4;
-        Serial.print(pinByte);
-        if (pinByte <= split) {  // brake range; reverse output
-            pinByte = (pinByte - split) >= 0 ? (pinByte - split) : -(pinByte - split);
-            Joystick.setYAxis(pinByte);
-        } else {
-            Joystick.setThrottle(pinByte);
-        }
-    } else {
-        pinByte = 0;
-        Joystick.setThrottle(pinByte);
-        Joystick.setYAxis(pinByte);
+void logger(String axis, int value) {
+  if (logs) {
+    Serial.print(axis);
+    Serial.print(" value = ");
+    Serial.print(value);
+    Serial.print('\n');
     }
 }
 
-/////////////////////////////////////////////
-// RUN:
-/////////////////////////////////////////////
 
-void loop() {
-    delay(2);  // delay value in ms, for read stability
-    setPin(dual);
+void setPin(int pin, int split=127) {
+    pinValue = analogRead(pin);
+    if (pinValue >= 0) {
+      pinByte = pinValue / 4;
+        if (pinByte <= split) {  
+            // brake range (1 - 254); reverse output
+            pinByte = (pinByte - split) >= 0 ? (pinByte - split) : -(pinByte - split);
+            Joystick.setYAxis(pinByte * 2);
+            logger("Brake", pinByte * 2);
+        } else {
+            // throttle range (1 - 254); limit to max bit value of 254;
+            pinByte = (pinByte - split) > 254 ? (pinByte - split) : 254;
+            Joystick.setThrottle(pinByte);
+            logger("Throttle", pinByte);
+        }
+    }
 }
 
 
+// run: //
+void setup() {
+    Serial.begin(9600);
+    Joystick.begin();
+}
 
-
-
-
+void loop() {
+    delay(10);  // delay value in ms, for read stability
+    setPin(dual);
+}
