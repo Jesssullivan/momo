1cf9c13a18142eaeb0d1a74a32ef451c1885bfb0
Jess Sullivan <jess@sulliwood.org>
2020-03-05 12:29:59 -0500

    ...

--- utb/main.h
+++ utb/main.h
@@ -1,20 +1,18 @@
 
 #include <Joystick.h>
 #include <Encoder.h>
+Joystick_ Joystick;
 
 Encoder enc(8, 9);
 
-
 // use Serial monitor?
-bool logs = true;
+bool logs = false;
 
-unsigned long time_a = millis();
-
-void setup() {
-    Serial.begin(9600);
-}
+// read detent:
+float variance = 22;
 
 // serial logging:
+unsigned long time_a = millis();
 void lprint(String text) {
     if (logs) {
         if (time_a - millis() >= 500) {
@@ -25,15 +23,43 @@ void lprint(String text) {
     }
 }
 
-float oldPosition;
+float cuRead;
+
+void setEncoder() {
+    float encRead = enc.read();
+    cuRead = encRead != cuRead ? encRead : cuRead;
+        if (cuRead > 0) {
+            if (cuRead >= variance) {
+                float cuRead_mx = cuRead * 1.5;
+                cuRead_mx = cuRead_mx >= 256 ? 256 : cuRead_mx;
+                lprint("Throttle = " + String(cuRead_mx) + "\n");
+                Joystick.setThrottle(cuRead_mx);
+                } else {
+                    Joystick.setThrottle(0);
+                    Joystick.setYAxis(0);
+                    lprint("Resting... \n");
+                }
+        } else {
+            cuRead = cuRead < 0 ? -encRead : encRead;
+            if (cuRead >= variance) {
+                float cuRead_mx = cuRead * 1.6;
+                cuRead_mx = cuRead_mx >= 256 ? 256 : cuRead_mx;
+                lprint("Brake = " + String(cuRead_mx) + "\n");
+                Joystick.setYAxis(cuRead_mx);
+            }  else {
+                Joystick.setThrottle(0);
+                Joystick.setYAxis(0);
+                lprint("Resting... \n");
+            }
+        }
+}
+
+void setup() {
+    //Joystick_ Joystick;
+    Joystick.begin();
+    Serial.begin(9600);
+}
 
 void loop() {
-    float newPosition;
-    newPosition = enc.read();
-    if (newPosition != oldPosition) {
-        lprint(String(newPosition));
-        oldPosition = newPosition;
-    }
-    //Serial.print(newPosition);
-    lprint(String(newPosition));
+    setEncoder();
 }
